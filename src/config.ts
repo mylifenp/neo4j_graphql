089e42a0b5b274895e433ca256defcadbc717265
Amit Shah <shah@vistra.eu>
2022-08-12 09:41:50 +0200

    WIP

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 
+export const ENV = process.env.ENV || "development"
 export const PORT = process.env.PORT || 8080
 export const NEO4J_URI = process.env.NEO4J_URI || "";
 export const NEO4J_USER = process.env.NEO4J_USER || "";
--- src/driver/index.ts
+++ src/driver/index.ts
@@ -1,8 +1,15 @@
 import "dotenv/config";
 import neo4j from "neo4j-driver";
-import { NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD } from "../config";
+import { NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD, ENV } from "../config";
 
-
+// const driver = () => {
+//   if (ENV === "development") {
+//     return neo4j.driver(
+//       NEO4J_URI,
+//       neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD)
+//     );
+//   }
+// };
 export default neo4j.driver(
   NEO4J_URI,
   neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD)
